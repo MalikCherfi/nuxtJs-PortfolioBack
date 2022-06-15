076d666d1779cc87642149b0d883cba615b6ca54
Malik Cherfi <malikcherfi@gmail.com>
2022-06-15 12:40:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const port = 8000;
+
+const cors = require("cors");
+
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+const multer = require("multer");
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../nuxtTestBack/public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+const cpUpload = upload.fields([
+  { name: "image.principal", maxCount: 1 },
+  { name: "image.secondary", maxCount: 10 },
+]);
+
+app.post("/upload", cpUpload, (req, res) => {
+  console.log(req.files);
+
+  res.send([
+    { principal: req.files["image.principal"][0].filename },
+    { secondary: req.files["image.secondary"].map((e) => e.filename) },
+  ]);
+});
+
+app.post("/post", (req, res) => {
+  const description = req.body;
+  console.log(description);
+  prisma.post
+    .create({ data: { ...description } })
+    .then((createdProject) => {
+      res.status(201).json(createdProject);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving project");
+    });
+});
+
+app.get("/post", (req, res) => {
+  prisma.post
+    .findMany()
+    .then((project) => {
+      res.status(201).send(project);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving project from database");
+    });
+});
+
+app.get("/post/:id", (req, res) => {
+  const { id } = req.params;
+  prisma.post
+    .findUnique({ where: { id: parseInt(id, 10) } })
+    .then((project) => {
+      console.log(project);
+      res.status(200).send(project);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error retrieving project from database");
+    });
+});
+
+app.delete("/post/:id", (req, res) => {
+  const { id } = req.params;
+  prisma.post
+    .delete({ where: { id: parseInt(id, 10) } })
+    .then(() => {
+      res.status(201).send("Project deleted with success");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error deleting the project");
+    });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
---
+++ prisma/migrations/20220601123552_migration2/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `published` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `images` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Post` DROP FOREIGN KEY `Post_authorId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `Profile` DROP FOREIGN KEY `Profile_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `Post` DROP COLUMN `authorId`,
+    DROP COLUMN `content`,
+    DROP COLUMN `createdAt`,
+    DROP COLUMN `published`,
+    DROP COLUMN `updatedAt`,
+    ADD COLUMN `details` VARCHAR(191) NULL,
+    ADD COLUMN `images` JSON NOT NULL,
+    ADD COLUMN `years` INTEGER NOT NULL DEFAULT 1900;
+
+-- DropTable
+DROP TABLE `Profile`;
