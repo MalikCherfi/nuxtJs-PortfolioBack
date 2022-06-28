37d2e2a68bc149fc63a5b06a814b80c4e1342415
Malik Cherfi <malikcherfi@gmail.com>
2022-06-28 16:17:30 +0200

    change schema prisma for cascade ondelete

---
+++ prisma/migrations/20220628130939_change_to_cascade/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE `TechnologiesOnPosts` DROP FOREIGN KEY `TechnologiesOnPosts_postId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `TechnologiesOnPosts` DROP FOREIGN KEY `TechnologiesOnPosts_technologyId_fkey`;
+
+-- AddForeignKey
+ALTER TABLE `TechnologiesOnPosts` ADD CONSTRAINT `TechnologiesOnPosts_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TechnologiesOnPosts` ADD CONSTRAINT `TechnologiesOnPosts_technologyId_fkey` FOREIGN KEY (`technologyId`) REFERENCES `Technology`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
